e26543df8e25fabed4cc715c6e83f70d6b96c9fb
wangyang-o <1370228031@qq.com>
2021-02-01 23:55:16 +0800

    promiseAll完善

--- 手撕代码/封装promiseAll.js
+++ 手撕代码/封装promiseAll.js
@@ -4,15 +4,19 @@
  * @Author: wy
  * @Date: 2021年01月12日 15:44:13
  * @LastEditors: wy
- * @LastEditTime: 2021年01月12日 15:54:37
+ * @LastEditTime: 2021年02月01日 23:53:37
  */
 function promiseAll(promises) {
     return new Promise((reslove, reject) => {
         const result = [];
-        promises.forEach(item => {
+        if (!Array.isArray(promises)) {
+            reject('error');
+        }
+        promises.forEach((item, index) => {
             item.then(
                 res => {
-                    result.push(res);
+                    // 保证先后顺序
+                    result[index] = res;
                     if (result.length === promises.length) {
                         reslove(result);
                     }
@@ -24,3 +28,26 @@ function promiseAll(promises) {
         });
     })
 }
+
+// 函数返回的是一个 promise
+let fn1 = function () {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('fn1');
+        }, 400)
+    })
+}
+let fn2 = function (fn) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('fn2');
+        }, 200)
+    })
+}
+
+promiseAll([fn1(), fn2()]).then(res => {
+    console.log(res); // [ 'fn1', 'fn2' ]
+}, rej => {
+    console.log(rej); // [ 'fn1', 'fn2' ]
+
+})
