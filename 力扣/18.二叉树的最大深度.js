50d6b1f5a7c4b8b99c7f50f4ecbb844620dbc9c5
wangyang-o <1370228031@qq.com>
2021-02-20 23:53:41 +0800

    二叉树的最大深度

---
+++ 力扣/18.二叉树的最大深度.js
@@ -0,0 +1,29 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: wy
+ * @Date: 2021年01月26日 14:46:29
+ * @LastEditors: wy
+ * @LastEditTime: 2021年02月20日 23:53:25
+ */
+// 题目：二叉树的最大深度
+
+// 思路：递归
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+    if (!root) return 0;
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    //  return root ? Math.max(maxDepth(root.left), maxDepth(root.right)) + 1 : 0;
+};
